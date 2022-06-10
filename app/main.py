d3d63b930f876d645cf06bd76ee6a4b50d8063e1
Ilan Emanuel Fritzler <contacto.fritzlerilan@gmail.com>
2022-06-10 01:16:30 -0300

    adding connection and startup database

--- app/main.py
+++ app/main.py
@@ -3,6 +3,10 @@
 """
 import uvicorn
 from fastapi import FastAPI
+from sqlmodel import SQLModel
+from app.db.db import engine
+
+from app.models.user import User
 
 app = FastAPI()
 
@@ -10,11 +14,18 @@ app = FastAPI()
 @app.get("/")
 def root():
     """Root Endpoint"""
-    return "hello world"
+    return {"message": "Hello World"}
+
+
+def create_db_and_tables():
+    """Create all data tables in database"""
+    SQLModel.metadata.create_all(engine)
+    print("Database created")
 
 
 def start():
     """Start app function for poetry script"""
+    create_db_and_tables()
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
