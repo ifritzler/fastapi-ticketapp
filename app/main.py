87b188f0c8d73ee6159630907660c1195fc2fc3e
Ilan Emanuel Fritzler <contacto.fritzlerilan@gmail.com>
2022-06-09 23:16:44 -0300

    fix/ poetry venv and startup project config

---
+++ app/main.py
@@ -0,0 +1,29 @@
+"""
+    Hola esta es una descripcion del modulo sin empezar :)
+"""
+from imp import reload
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    """Root Endpoint"""
+    return "hello world"
+
+
+def start():
+    """Start app function for poetry script"""
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+        workers=2,
+    )
+
+
+if __name__ == "__main__":
+    start()
--- main.py
+++
@@ -1,4 +0,0 @@
-"""
-    Hola esta es una descripcion del modulo sin empezar :)
-"""
-print('Hello world')
